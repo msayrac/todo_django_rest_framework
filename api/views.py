2d0aee699847c433004623a273c5543912bd89c8
msayracode <msayracode@gmail.com>
2026-03-13 22:45:11 +0300

    create item added to front end and talk to api

--- api/views.py
+++ api/views.py
@@ -20,7 +20,7 @@ def apiOverview(request):
 
 @api_view(['GET'])
 def taskList(request):
-    task = Task.objects.all()
+    task = Task.objects.all().order_by('-id')
     serializer = TaskSerializer(task, many=True)
     return Response(serializer.data)
 
